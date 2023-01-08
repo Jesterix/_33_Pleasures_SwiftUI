4607391adb4a5d26b7634af6ce4dfc86ea12f7bc
George Khaydenko <ghaidenko@gmail.com>
2023-01-08 13:44:23 +0300

    Initial Commit

---
+++ _33_Pleasures_SwiftUI/_33_Pleasures_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _33_Pleasures_SwiftUIApp.swift
+//  _33_Pleasures_SwiftUI
+//
+//  Created by Георгий on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct _33_Pleasures_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
